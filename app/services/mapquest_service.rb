2aaad7311bd2a2836d9bd53671b244e798835fb0
matthewjholmes <ilookup@gmail.com>
2022-02-05 16:24:38 -0700

    Scaffold of zip code search methods in facade, model, and service

---
+++ app/services/mapquest_service.rb
@@ -0,0 +1,20 @@
+class MapquestService
+
+  def self.route(location1, location2)
+    call_api("/directions/v2/route?from=#{location1}&to=#{location2}")
+  end
+
+  def self.call_api(path)
+    response = conn.get(path)
+    parse(response)
+  end
+
+  def self.parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.conn
+    Faraday.new(url: "http://www.mapquestapi.com", params: { key: ENV['MAPQUEST_API_KEY'] })
+  end
+
+end
