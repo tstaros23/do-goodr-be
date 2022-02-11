3cd325f29aa6ee53cb6fe93c5c6e29a80f774519
Matt Holmes <78521446+matthewjholmes@users.noreply.github.com>
2022-02-11 16:33:23 -0700

    Test for org events

--- app/controllers/api/v1/org_events_controller.rb
+++ app/controllers/api/v1/org_events_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::OrgEventsController < ApplicationController
 
   def index
-    org = Organization.find(params[:organization_id])
-    events = Event.organization_filter(org.id)
+    events = Event.organization_filter(params[:organization_id])
     j = render json: EventSerializer.format_multiple(events), status: :ok
   end
 
