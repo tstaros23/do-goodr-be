76cf6c21a737d4b4a3e00d38104da8e2d034d674
Ted Staros <tstaros23@gmail.com>
2022-02-14 16:07:18 -0700

    Create mail tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'simplecov'
+SimpleCov.start
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
@@ -7,8 +10,6 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 # require 'webmock/rspec'
-require 'simplecov'
-SimpleCov.start
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
