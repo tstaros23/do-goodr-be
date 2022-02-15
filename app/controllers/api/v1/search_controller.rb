e8533c368b1c09f4e9c946e48f3ac17982a7c99b
Ted Staros <tstaros23@gmail.com>
2022-02-15 10:47:38 -0700

    Fix error handling

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -2,6 +2,10 @@ class Api::V1::SearchController < ApplicationController
 
   def index
     events = Event.distance_filter(params[:zip], params[:distance])
-    j = render json: EventSerializer.format_search(events), status: :ok
+    if events.empty? || events.include?(nil)
+      render json: {errors: {details: "Invalid zip code"}}, status: :bad_request
+    else
+      render json: EventSerializer.format_search(events), status: :ok
+    end
   end
 end
